732e4bcefccd94b4b9beb30adebff79a9e3c3ef7
Araguntajyothi <Aragunta.Jyothi@cognizant.com>
2020-08-19 18:29:54 +0530

    multiplication

---
+++ JAVA/src/java/MultiplictionTableRunner.java
@@ -0,0 +1,12 @@
+package java;
+
+public class MultiplictionTableRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MultiplicationTable table=new MultiplicationTable();
+		table.print();
+
+	}
+
+}
